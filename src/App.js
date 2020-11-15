a6632f6dc793563710ccf631c6eee52325581edc
Arvin Behshad <arvin@gimitheapp.com>
2020-11-15 12:50:04 +0100

    v0.1: pre-skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { notes } from './helpers/notes'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Row>
+        <Col xs={12} md={4}>
+          <ul>
+            {notes.map((note) => (
+              <li>{note}</li>
+            ))}
+          </ul>
+        </Col>
+        <Col xs={12} md={8}>
+          Editor
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
-export default App;
+export default App
---
+++ src/helpers/notes.js
@@ -0,0 +1,18 @@
+/*
+C: create a note
+R: Read (get) one note and all notes
+U: Update a note
+D: Delete a note
+*/
+
+export const notes = []
+
+export function createNote() {}
+
+export function getNote() {}
+
+export function getNotes() {}
+
+export function updateNote() {}
+
+export function deleteNote() {}
